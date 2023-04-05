9e55962c0500a3290b85290e4439cde77a7b36ff
Nicolas-tec <71329970+Nicolas-tec@users.noreply.github.com>
2023-04-05 17:04:59 -0500

    Create historial.php

---
+++ php/historial.php
@@ -0,0 +1,13 @@
+<?php
+include_once('db.php');
+$clave = $_POST['clave'];
+$N_publicasion = $_POST['N_publicasion'];
+$N_historia = $_POST['N_historia'];
+$historia = $_POST['historia'];
+$conn=conn();
+$sql = "INSERT INTO historias (id_histroria, clave, N_publicasion, N_historia, historia)
+VALUES ('', '$clave', '$N_publicasion', '$N_historia', '$historia')";
+$resul = mysqli_query($conn, $sql)or trigger_error("Query Failed! SQL- Error:".mysqli_error($conn),E_USER_ERROR);
+echo "$sql";
+header("location: ../html/historia.html");
+?>
